7e105995758f511e3b96e46f7623b5e48c2efe4e
Igor <igrr69@gmail.com>
2018-11-18 11:52:37 +0200

    Fix email format
    Add destination input

--- bot.py
+++ bot.py
@@ -4,10 +4,11 @@ import smtplib, getpass
 
 saved = {}
 
-def check_for_updates():
+
+def check_for_updates(user, password, destination):
     current = {}
     for submission in reddit.subreddit('buildapcsales').search(query='GPU', sort='new', limit=10):
-        current[submission.id] = {submission.title: submission.shortlink}
+        current[submission.id] = '{}: {}'.format(submission.title, submission.shortlink)
 
     global saved
     if len(saved) == 0:
@@ -19,19 +20,20 @@ def check_for_updates():
         if len(difference) != 0:
             for id in difference:
                 print(current[id])  # this is what going to be sent by mail
+                set_smtp_connection(user, password, destination, current[id])
                 saved[id] = current[id]
             ids_to_remove = saved_ids - current_ids
             for id in ids_to_remove:
                 del saved[id]
-    print(saved)
 
-def set_smtp_connection(user, password, message):
+
+def set_smtp_connection(user, password, destination, message):
     smtpObj = smtplib.SMTP('smtp-mail.outlook.com', 587)
     smtpObj.ehlo()
     smtpObj.starttls()
     try:
         smtpObj.login(user, password)
-        smtpObj.sendmail(user, user, 'Subject: GPU sale update.\n\nSent from GPU-BOT')
+        smtpObj.sendmail(user, destination, 'Subject: GPU sale update.\n\n{}\n\nSent from GPU-BOT'.format(message))
     except smtplib.SMTPAuthenticationError:
         print('Wrong username or password')
     smtpObj.quit()
@@ -41,14 +43,11 @@ if __name__ == '__main__':
     reddit = praw.Reddit(client_id='nRhqSW1S9WfOdA', client_secret='CeQhsCuWYFinErouREl54koA89c', user_agent='bot')
 
     user = input('Email: ')
-    password = input('Password: ')
-    # password = getpass.getpass('Password: ')
+    password = getpass.getpass()
+    destination = input('Destination: ')
 
-    set_smtp_connection(user, password, 'HelloWorld!')
-
-    # check_for_updates()
+    check_for_updates(user, password, destination)
     # print(saved)
-    # while 1:
-    #     time.sleep(900)
-    #     check_for_updates()
-
+    while 1:
+        time.sleep(900)
+        check_for_updates(user, password, destination)

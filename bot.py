b1a11c36b44a134b21303ec2ae2ea6a8cab6b436
Igor <igrr69@gmail.com>
2018-11-20 11:42:34 +0200

    Add usage of .env file
    A new bot was made with new keys
    
    Ready for production!

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import praw
 import time, os
 import smtplib, getpass
+from dotenv import load_dotenv
 
 
 def check_for_updates(user, password, destination):
@@ -17,8 +18,9 @@ def check_for_updates(user, password, destination):
         difference = current_ids - saved_ids
         if len(difference) != 0:
             for id in difference:
-                print(current[id])  # this is what going to be sent by mail
+                print('A new sale was posted! Sending email...')  # this is what going to be sent by mail
                 set_smtp_connection(user, password, destination, current[id])
+                print('Email was sent!')
                 saved[id] = current[id]
             ids_to_remove = saved_ids - current_ids
             for id in ids_to_remove:
@@ -31,23 +33,30 @@ def set_smtp_connection(user, password, destination, message):
     smtpObj.starttls()
     try:
         smtpObj.login(user, password)
-        smtpObj.sendmail(user, destination, 'Subject: GPU sale update.\n\n{}\n\nSent from GPU-BOT'.format(message))
+        if message != 'check101':   # check connection - no need to send email yet
+            smtpObj.sendmail(user, destination, 'Subject: GPU sale update.\n\n{}\n\nSent from GPU-BOT'.format(message))
     except smtplib.SMTPAuthenticationError:
         print('Wrong username or password')
     smtpObj.quit()
 
 
 if __name__ == '__main__':
+
+    path = os.path.join(os.getcwd(),'.env')
+    load_dotenv(path)
+
     reddit = praw.Reddit(client_id=os.environ['CLIENT_ID'], client_secret=os.environ['CLIENT_SECRET'], user_agent='bot')
 
-    user = input('Email: ')
-    password = getpass.getpass()
-    destination = input('Destination: ')
+    user = os.environ['USER_EMAIL']
+    password = os.environ['USER_PASS']
+    destination = os.environ['DESTINATION']
+
+    set_smtp_connection(user,password,destination,'check101')   # check email credentials
 
     saved = {}
 
     check_for_updates(user, password, destination)
-    print('Bot has started!')
+    print('Bot has started! No errors')
 
     while 1:
         time.sleep(900)
